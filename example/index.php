d1b7976b12f75707991b10ebf157ba958e2b7635
miguelangel <miguelangel.jaimen@gmail.com>
2017-11-24 18:43:12 -0300

    primer commit

---
+++ example/index.php
@@ -0,0 +1,8 @@
+<?php 
+require_once(dirname(__FILE__) . "/../vendor/autoload.php");
+$prueba = new \bla\hello\Hello();
+$prueba->sayHello("juan");
+
+
+
+ ?>
---
+++ src/hello.php
@@ -0,0 +1,15 @@
+<?php 
+namespace bla\hello;
+
+class Hello
+{
+	
+	public function sayHello($nombre)
+	{
+		echo "hello $nombre";
+	}
+
+
+}
+
+ ?>
